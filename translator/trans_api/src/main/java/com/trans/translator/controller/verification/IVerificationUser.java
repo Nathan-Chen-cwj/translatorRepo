548b224767a7203865c49a69ed6dad55db5c5cc8
Nathan <1121369106@qq.com>
2021-07-12 23:50:18 +0800

    引入swagger导致程序启动异常，是jre缺少Google的一个jar包的问题；另腾讯sdk包确实存在问题，解决：回退到3的腾讯sdk版本依赖；另设想了IVerificationUser接口备注的三个需求

---
+++ translator/trans_api/src/main/java/com/trans/translator/controller/verification/IVerificationUser.java
@@ -0,0 +1,13 @@
+package com.trans.translator.controller.verification;
+
+/**
+ * @Version 1.0
+ * @Author NathanChen
+ * @Date 2021/7/12 23:39
+ *  1、使用拦截器来进行用户登陆校验以及授权
+ *  2、使用自定义注解来验证用户名是否已被使用，用户手机号码是否已被使用，用户邮箱是否已被使用
+ *  3、创建线程池，来实现并发编程的需求
+ *
+ */
+public interface IVerificationUser {
+}
